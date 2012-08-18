e25d7ca363de84a918067099c146f15e901000c4
Janos Gyerik <janos.gyerik@titan2x.com>
2012-08-18 15:22:26 +0200

    added col.sh: extract n-th column of stdin

---
+++ bash/col.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+# SCRIPT: col.sh
+# AUTHOR: Janos Gyerik <info@janosgyerik.com>
+# DATE:   2012-08-18
+# REV:    1.0.D (Valid are A, B, D, T and P)
+#               (For Alpha, Beta, Dev, Test and Production)
+#
+# PLATFORM: Not platform dependent
+#
+# PURPOSE: Extract the n-th column of stdin
+#
+# set -n   # Uncomment to check your syntax, without execution.
+#          # NOTE: Do not forget to put the comment back in or
+#          #       the shell script will not execute!
+# set -x   # Uncomment to debug this shell script (Korn shell only)
+#
+
+usage() {
+    test $# = 0 || echo $@
+    echo "Usage: $0 [OPTION]... COL"
+    echo
+    echo Extract the n-th column of stdin
+    echo
+    echo Options:
+    echo
+    echo "  -h, --help       Print this help"
+    echo
+    exit 1
+}
+
+args=
+#arg=
+#flag=off
+#param=
+while [ $# != 0 ]; do
+    case $1 in
+    -h|--help) usage ;;
+#    -f|--flag) flag=on ;;
+#    --no-flag) flag=off ;;
+#    -p|--param) shift; param=$1 ;;
+#    --) shift; while [ $# != 0 ]; do args="$args \"$1\""; shift; done; break ;;
+    -) usage "Unknown option: $1" ;;
+    -?*) usage "Unknown option: $1" ;;
+    *) args="$args \"$1\"" ;;  # script that takes multiple arguments
+#    *) test "$arg" && usage || arg=$1 ;;  # strict with excess arguments
+#    *) arg=$1 ;;  # forgiving with excess arguments
+    esac
+    shift
+done
+
+eval "set -- $args"  # save arguments in $@. Use "$@" in for loops, not $@ 
+
+test $# -gt 0 || usage
+
+awk '{print $'$(echo $* | sed -e 's/ /,$/g')'}'
+
+# eof
