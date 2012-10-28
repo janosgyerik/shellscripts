2f81ae9c775a2e015f8b7e728346f63b8aca318f
Janos Gyerik <info@titan2x.com>
2012-10-28 10:41:05 +0100

    added script to rename files by adding a numeric prefix to achieve ordering as listed in a list file

---
+++ python/reorder.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import os
+#import re
+
+
+def newname(name, i):
+    # todo: format controls
+    return '%02d-%s' % (i, name)
+
+
+def matching(files, pattern):
+    # todo: matching logic control: regex, case insensitive
+    return [x for x in files if x == pattern]
+    #return [x for x in files if x.endswith(pattern)]
+    #return [x for x in files if re.search(pattern, x)]
+
+
+def reorder(path, listfile):
+    files = os.listdir(path)
+    patterns = [x.strip() for x in listfile.readlines()]
+    i = 0
+    for pattern in patterns:
+        print '# %s -> ...' % pattern
+        for name in matching(files, pattern):
+            if os.path.exists(name):
+                i += 1
+                print 'mv "%s" "%s"' % (name, newname(name, i))
+        print
+
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.set_usage('%prog [options] LISTFILE...')
+    '''
+    parser.add_option('-r', '--regex', action='store_true', default=False,
+            help='use regular expressions to find matching files')
+    '''
+    parser.set_description('Add numeric prefix to filenames to match order specified in a list file')
+    (options, args) = parser.parse_args()
+
+    if args:
+        for path in args:
+            if os.path.isfile(path):
+                reorder('.', open(path))
+    else:
+        parser.print_help()
+
+# eof
