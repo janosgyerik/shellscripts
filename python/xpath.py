92246291c423f39c517389551d69fc16608d3ff0
Janos Gyerik <janos@xw8400>
2013-10-06 09:17:43 +0200

    added xpath.py to test xpath expressions

---
+++ python/xpath.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import argparse
+import libxml2
+
+example = '''Example file:
+
+    <nodes>
+       <node at1="1" at2="2"> 12 </node>
+       <node at1="1" at2="2" at3="3"> 123 </node>
+       <node at1="1"> 1 </node>
+    </nodes>
+
+Example xpath: //node[@at1 and count(@*) = 1]
+'''
+
+parser = argparse.ArgumentParser(description='Test on an XML file an XPATH expression')
+parser.add_argument('file', help='path to an xml file')
+parser.add_argument('path', help='an xpath expression')
+parser.add_argument('--example', help='print an example', action='store_true')
+
+args = parser.parse_args()
+
+if args.example:
+    print example
+    parser.exit()
+
+xmlfile = args.file
+path = args.path
+
+doc = libxml2.parseFile(xmlfile)
+ctx = doc.xpathNewContext()
+print '# path:', path
+for match in ctx.xpathEval(path):
+    print match
