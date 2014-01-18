c405aef4de0ed7c60bc90c51e12dab45b22733af
Janos Gyerik <info@janosgyerik.com>
2014-01-18 09:11:12 +0100

    added repo_start and repo_end and improved pending cmd

--- bash/git.sh
+++ bash/git.sh
@@ -93,8 +93,15 @@ print_heading() {
 
 warn() {
     print_heading
-    warnings=1
     echo '  [W]' $*
+}
+
+repo_start() {
+    printed_heading=
+}
+
+repo_end() {
+    test "$printed_heading" && echo
 }
 
 case $command in
@@ -102,31 +109,27 @@ case $command in
         repolist "$@"
         ;;
     pending)
-        for dir; do
-            test -d "$dir" || continue
-            for repo in $(repolist "$dir"); do
-                printed_heading=
-                warnings=
-                pending=
-                (cd $repo; git remote | grep -x origin >/dev/null) || warn 'origin not defined'
-                (cd $repo; grep remote.*=.*origin .git/config >/dev/null) || warn 'origin not set as upstream; git branch --set-upstream-to origin/master'
-                (cd $repo; git symbolic-ref HEAD | grep -x -e refs/heads/master -e refs/heads/gh-pages >/dev/null) || warn 'not on master or gh-pages'
-                (cd $repo; git status | grep 'Your branch is ahead of' >/dev/null) && warn 'ahead of tracked branch'
-                (cd $repo; git status | grep 'Changes to be committed' >/dev/null) && warn 'staged but uncommitted changes' && pending=1
-                (cd $repo; git status | grep 'Changes not staged' >/dev/null) && warn 'unstaged pending changes' && pending=1
-                (cd $repo; git status | grep 'Untracked files' >/dev/null) && warn 'untracked files' && pending=1
-                test "$pending" || (cd $repo; git status | grep 'working directory clean' >/dev/null) || warn 'other (??) pending changes'
-                test "$printed_heading" && echo
-            done
+        repolist "$@" | while read repo; do
+            repo_start
+            pending=
+            (cd $repo; git remote | grep -x origin >/dev/null) || warn 'origin not defined'
+            (cd $repo; grep remote.*=.*origin .git/config >/dev/null) || warn 'origin not set as upstream; git branch --set-upstream-to origin/master'
+            (cd $repo; git symbolic-ref HEAD | grep -x -e refs/heads/master -e refs/heads/gh-pages >/dev/null) || warn 'not on master or gh-pages'
+            (cd $repo; git status | grep 'Your branch is ahead of' >/dev/null) && warn 'ahead of tracked branch'
+            (cd $repo; git status | grep 'Changes to be committed' >/dev/null) && warn 'staged but uncommitted changes' && pending=1
+            (cd $repo; git status | grep 'Changes not staged' >/dev/null) && warn 'unstaged pending changes' && pending=1
+            (cd $repo; git status | grep 'Untracked files' >/dev/null) && warn 'untracked files' && pending=1
+            test "$pending" || (cd $repo; git status | grep 'working directory clean' >/dev/null) || warn 'other (??) pending changes'
+            repo_end
         done
         ;;
     outdated)
         repolist "$@" | while read repo; do
-            printed_heading=
+            repo_start
             test $fetch = on && GIT_DIR=$repo/.git git fetch origin >/dev/null 2>/dev/null
             behind=$(GIT_DIR="$repo"/.git git status | sed -ne 's/.* behind .* by \([0-9]*\) commit.*/\1/p')
             test "$behind" && warn "behind by $behind commit(s)"
-            test "$printed_heading" && echo
+            repo_end
         done
         ;;
     *) usage ;;
