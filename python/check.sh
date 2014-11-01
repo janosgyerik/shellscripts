251f8732ecec81344396dc545ea85f1de80da693
Janos Gyerik <janos@kronos>
2014-11-01 19:29:18 +0100

    improved check script

--- python/check.sh
+++ python/check.sh
@@ -2,12 +2,20 @@
 
 cd $(dirname "$0")
 
+# E121 continuation line indentation is not a multiple of four
+# E123 closing bracket does not match indentation of opening bracket's line
+# E126 continuation line over-indented for hanging indent
+# E128 continuation line under-indented for visual indent
+# E501 line too long > 79 characters
 echo '### pep8'
-pep8 . | grep -v 'line too long'
+pep8 . | grep -v \
+    -e E121 \
+    -e E123 \
+    -e E126 \
+    -e E128 \
+    -e E501
 echo
 
 echo '### pyflakes'
 pyflakes .
 echo
-
-# eof
