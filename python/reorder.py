7f6c259cf34040ab9519bf3bfa77cee932743775
Janos Gyerik <janos@kronos>
2014-11-01 20:11:44 +0100

    simplified logic of reorder

--- python/reorder.py
+++ python/reorder.py
@@ -2,30 +2,20 @@
 
 from optparse import OptionParser
 import os
-# import re
 
 
 def newname(name, i):
-    # todo: format controls
     return '%02d-%s' % (i, name)
 
 
-def matching(files, pattern):
-    # todo: matching logic control: regex, case insensitive
-    return [x for x in files if x == pattern]
-    # return [x for x in files if x.endswith(pattern)]
-    # return [x for x in files if re.search(pattern, x)]
-
-
-def reorder(path, listfile):
-    files = os.listdir(path)
-    patterns = [x.strip() for x in listfile.readlines()]
+def reorder(listfile):
     i = 0
-    for pattern in patterns:
-        for name in matching(files, pattern):
-            if os.path.exists(name):
+    with open(listfile) as fh:
+        for line in fh:
+            filename = line.strip()
+            if os.path.exists(filename):
                 i += 1
-                print('mv "%s" "%s"' % (name, newname(name, i)))
+                print('mv "%s" "%s"' % (filename, newname(filename, i)))
 
 
 def main():
@@ -39,9 +29,9 @@ def main():
     (options, args) = parser.parse_args()
 
     if args:
-        for path in args:
-            if os.path.isfile(path):
-                reorder('.', open(path))
+        for listfile in args:
+            if os.path.isfile(listfile):
+                reorder(listfile)
     else:
         parser.print_help()
 
