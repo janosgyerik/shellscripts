976a85d320c2a8f9176f1e400b240b5c645ce260
Janos Gyerik <janos@kronos>
2014-11-02 21:56:33 +0100

    moved all code inside functions

--- python/free.py
+++ python/free.py
@@ -21,33 +21,36 @@ def get_rss_total():
             rss_total += float(rss) * 1024
     return rss_total
 
-rssTotal = get_rss_total()
 
-
-def get_vm_stat():
-    vm = subprocess.Popen(['vm_stat'], stdout=subprocess.PIPE).communicate()[0]
-    vmLines = vm.split('\n')
-    sep = re.compile(':[\s]+')
-    vmStats = {}
-    for row in range(1, len(vmLines) - 2):
-        rowText = vmLines[row].strip()
-        rowElements = sep.split(rowText)
-        vmStats[(rowElements[0])] = int(rowElements[1].strip('\.')) * 4096
-    return vmStats
-
-vmStats = get_vm_stat()
+def get_vmstats():
+    vmstat_out = subprocess.Popen(['vm_stat'], stdout=subprocess.PIPE).communicate()[0]
+    re_key_value = re.compile(r'([^:]+).*?(\d+)')
+    vmstats = {}
+    for line in vmstat_out.split('\n'):
+        m = re_key_value.match(line)
+        if m:
+            key, value = m.groups()
+            vmstats[key] = int(value) * 4096
+    return vmstats
 
 
 def print_item(label, value):
     print('{:24}{} MB'.format(label + ':', value))
 
 
-def print_vmstat_item(label, key):
-    print_item(label, vmStats[key] / 1024 / 1024)
+def print_vmstat_item(vmstats, label, key):
+    print_item(label, vmstats[key] / 1024 / 1024)
 
 
-print_vmstat_item('Wired Memory', 'Pages wired down')
-print_vmstat_item('Active Memory', 'Pages active')
-print_vmstat_item('Inactive Memory', 'Pages inactive')
-print_vmstat_item('Free Memory', 'Pages free')
-print_item('Real Mem Total (ps)', '{:.3f}'.format(rssTotal / 1024 / 1024))
+def main():
+    vmstats = get_vmstats()
+    print_vmstat_item(vmstats, 'Wired Memory', 'Pages wired down')
+    print_vmstat_item(vmstats, 'Active Memory', 'Pages active')
+    print_vmstat_item(vmstats, 'Inactive Memory', 'Pages inactive')
+    print_vmstat_item(vmstats, 'Free Memory', 'Pages free')
+    print_vmstat_item(vmstats, 'Swapins', 'Swapouts')
+    rss_total = get_rss_total()
+    print_item('Real Mem Total (ps)', '{:.3f}'.format(rss_total / 1024 / 1024))
+
+if __name__ == '__main__':
+    main()
