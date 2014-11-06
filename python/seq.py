b7f990218abf4c3cb0e6e84b9d43112cdc38dfc6
Janos Gyerik <janos@kronos>
2014-11-07 00:06:25 +0100

    moved code to methods

--- python/seq.py
+++ python/seq.py
@@ -3,13 +3,20 @@
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Print sequences of numbers')
-parser.add_argument('first', nargs='?', default=1, type=int)
-parser.add_argument('incr', nargs='?', default=1)
-parser.add_argument('last', type=int)
+def seq(last, first=1, incr=1):
+    for num in range(first, last + 1, incr):
+        print(str(num))
 
-args = parser.parse_args()
 
-for num in range(args.first, args.last + 1):
-    print(str(num))
+def main():
+    parser = argparse.ArgumentParser(description='Print sequences of numbers, imitating the seq tool')
+    parser.add_argument('first', nargs='?', default=1, type=int)
+    parser.add_argument('incr', nargs='?', default=1, type=int)
+    parser.add_argument('last', type=int)
 
+    args = parser.parse_args()
+    seq(args.last, args.first, args.incr)
+
+
+if __name__ == '__main__':
+    main()
