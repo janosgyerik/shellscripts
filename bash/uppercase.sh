ca4320419dd28d0310623176b0b7d825dea4d3f6
Janos Gyerik <janos@kronos>
2014-11-13 22:19:40 +0100

    improved uppercase from lowercase

--- bash/uppercase.sh
+++ bash/uppercase.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 #
-# SCRIPT: upcase.sh
+# SCRIPT: uppercase.sh
 # AUTHOR: Janos Gyerik <info@janosgyerik.com>
 # DATE:   2004-12-23
 # REV:    1.0.T (Valid are A, B, D, T and P)
@@ -18,10 +18,12 @@
 #
 
 usage() {
-    test $# = 0 || echo $@
+    test $# = 0 || echo "$@"
     echo "Usage: $0 [OPTION]... FILE..."
     echo
     echo "Rename files to all uppercase letters."
+    echo
+    echo "  -n, --dry-run         Dry run, show what would happen"
     echo
     echo "  -h, --help            Print this help"
     echo
@@ -29,19 +31,14 @@ usage() {
 }
 
 args=
-#arg=
-#flag=off
-#param=
+dryrun=off
 while [ $# != 0 ]; do
     case $1 in
     -h|--help) usage ;;
-#    -f|--flag) flag=on ;;
-#    -p|--param) shift; param=$1 ;;
+    -n|--dry-run) dryrun=on ;;
     --) shift; while [ $# != 0 ]; do args="$args \"$1\""; shift; done; break ;;
     -?*) usage "Unknown option: $1" ;;
-    *) args="$args \"$1\"" ;;  # script that takes multiple arguments
-#    *) test "$arg" && usage || arg=$1 ;;  # strict with excess arguments
-#    *) arg=$1 ;;  # forgiving with excess arguments
+    *) args="$args \"$1\"" ;;
     esac
     shift
 done
@@ -50,11 +47,14 @@ eval "set -- $args"
 
 test $# -gt 0 || usage
 
-for i in "$@"; do
-    file=`basename "$i"`
-    dir=`dirname "$i"`
-    upcased=`echo "$file" | tr a-z A-Z`
-    old="$dir/$file"
-    new="$dir/$upcased"
-    test "$new" != "$old" && echo "\`$i' -> \`$new'" && mv -i -- "$i" "$new"
+for path; do
+    test -e "$path" || continue
+    origfile=$(basename "$path")
+    newfile=$(tr '[:lower:]' '[:upper:]' <<< "$origfile")
+    origdir=$(dirname "$path")
+    origpath=$origdir/$origfile
+    newpath=$origdir/$newfile
+    test "$newpath" != "$origpath" || continue
+    echo "$path -> $newpath"
+    test $dryrun = on || mv -i -- "$path" "$newpath"
 done
