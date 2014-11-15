247159c1a347658d02a12942fb2cd25eeddffd7b
Janos Gyerik <janos@kronos>
2014-11-15 22:59:04 +0100

    made pwgen work in both python2 and 3

--- python/pwgen.py
+++ python/pwgen.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
-from optparse import OptionParser
+from __future__ import print_function
+
 import random
 import string
 import re
+
+from optparse import OptionParser
 
 t_width = 80
 t_height = 25
 
 default_length = 12
 
-alphabet_default = string.letters + string.digits
+alphabet_default = string.ascii_letters + string.digits
 alphabet_complex = alphabet_default + '`~!@#$%^&*()_+-={}[];:<>?,./'
 alphabet_easy = re.sub(r'[l1ioO0Z2I]', '', alphabet_default)
 
@@ -18,7 +21,7 @@ double_letter = re.compile(r'(.)\1')
 
 
 def randomstring(alphabet, length=16):
-    return ''.join(random.choice(alphabet) for i in xrange(length))
+    return ''.join(random.choice(alphabet) for i in range(length))
 
 
 def has_double_letter(word):
@@ -33,22 +36,23 @@ def easy_to_type_randomstring(alphabet, length=16):
 
 
 def pwgen(alphabet, easy, length=16):
-    for i in xrange(t_height - 3):
-        for j in xrange(t_width / (length + 1)):
+    for i in range(t_height - 3):
+        for j in range(t_width // (length + 1)):
             if easy:
-                print easy_to_type_randomstring(alphabet, length),
+                print(easy_to_type_randomstring(alphabet, length), end=' ')
             else:
-                print randomstring(alphabet, length),
-        print
+                print(randomstring(alphabet, length), end=' ')
+        print()
+
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-a', '--alphabet',
-            help='override the default alphabet')
+                      help='override the default alphabet')
     parser.add_option('--complex', action='store_true', default=False,
-            help='use a very complex default alphabet', dest='complex_')
+                      help='use a very complex default alphabet', dest='complex_')
     parser.add_option('--easy', action='store_true', default=False,
-            help='use a simple default alphabet, with ambiguous characters removed and no doubled letters')
+                      help='use a simple default alphabet, with ambiguous characters removed and no doubled letters')
     parser.add_option('-l', '--length', type=int, default=default_length)
     parser.set_description('Generate random passwords')
     (options, args) = parser.parse_args()
