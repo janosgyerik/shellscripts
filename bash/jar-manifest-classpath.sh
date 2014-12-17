3afd9790636ea946c4d4dc3eb76d344f0d5976ed
Janos Gyerik <janos@kronos>
2014-12-17 23:38:23 +0100

    improved detection of TMPDIR

--- bash/jar-manifest-classpath.sh
+++ bash/jar-manifest-classpath.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if test -d "$TMPDIR"; then
+    :
+elif test -d "$TMP"; then
+    TMPDIR=$TMP
+elif test -d /var/tmp; then
+    TMPDIR=/var/tmp
+else
+    TMPDIR=/tmp
+fi
 workdir=$TMPDIR/$(basename "$0")-work-$$
 
 cleanup() {
