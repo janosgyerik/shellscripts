65976082d36762263f4ebe078f955a50baa00853
Janos Gyerik <janos@kronos>
2015-12-05 21:43:17 +0100

    added eggtimer script

---
+++ bash/eggtimer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if test $# -lt 2; then
+    echo "usage: $0 minutes seconds [message]"
+    exit 1
+fi
+
+min=$1
+sec=$2
+message=$3
+test "$message" || message='done'
+
+((target_time = $(date +%s) + min * 60 + sec))
+while :; do
+    ((current_time = $(date +%s)))
+    ((current_time >= target_time)) && break
+    ((remain_sec_total = target_time - current_time))
+    ((remain_min = remain_sec_total / 60))
+    ((remain_sec = remain_sec_total % 60))
+    printf '%4d:%02d  \r' $remain_min $remain_sec
+    sleep 1
+done
+
+printf '%-7s\n' "$message"
